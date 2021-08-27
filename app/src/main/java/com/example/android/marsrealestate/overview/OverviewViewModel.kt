f2a7ed52ec2dd99838b01f7011e4a6bb1de06d8c
Maxim <syromolotov.maks@mail.ru>
2021-08-27 14:21:18 +0300

    RecyclerView and adapters has been added

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -30,9 +30,9 @@ class OverviewViewModel : ViewModel() {
         get() = _status
 
 
-    private val _property=MutableLiveData<MarsProperty>()
-    val property:LiveData<MarsProperty>
-    get() = _property
+    private val _properties=MutableLiveData<List<MarsProperty>>()
+    val properties:LiveData<List<MarsProperty>>
+    get() = _properties
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -58,7 +58,7 @@ class OverviewViewModel : ViewModel() {
 
                 if(listResult.size>0)
                 {
-                    _property.value=listResult[0]
+                    _properties.value=listResult
                 }
             }
             catch(t:Throwable) {
